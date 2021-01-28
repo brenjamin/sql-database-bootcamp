008bba774488bee0689f0b5c093f2487f7a7829a
Ben <Ben@Kellys-MacBook-Pro-2.local>
2021-01-28 18:29:51 -0500

    Assignment 3 update

--- assignment-3/assignment-3.sql
+++ assignment-3/assignment-3.sql
@@ -1,11 +1,12 @@
 -- 0. Complete the lab to create the SELECT CREATE query!
-SELECT CONCAT('CREATE TABLE ', TABLE_NAME, ' (') AS query, 1 AS FILTER FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'taverns'
-UNION
-select CONCAT(COLUMN_NAME, ' ', DATA_TYPE, ','), 2
+SELECT CONCAT('CREATE TABLE ', TABLE_NAME, ' (') AS query FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'taverns'
+UNION ALL
+select CONCAT(COLUMN_NAME, ' ', DATA_TYPE,
+(CASE WHEN CHARACTER_MAXIMUM_LENGTH IS NOT NULL THEN  
+CONCAT('(', CAST(CHARACTER_MAXIMUM_LENGTH as varchar(100)), ')') ELSE '' END), ',')
 FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'taverns'
-UNION
-SELECT ');', 3
-ORDER BY FILTER;
+UNION ALL
+SELECT ');'
 
 -- 1. The system should also be able to track Rooms. Rooms should have a status and an associated tavern. 
 -- There should be a way to track Room Stays which will contain a sale, guest, room, the date it was stayed in and the rate
@@ -47,8 +48,20 @@ SELECT * from guests ORDER BY name ASC;
 SELECT TOP 10 * FROM supply_sales ORDER BY total_cost DESC
 
 -- 7. Write a query to return all the values stored in all Lookup Tables - Lookup tables are the tables we reference typically with just an ID and a name. This should be a dynamic combining of all of the tables
+SELECT * FROM locations
+UNION ALL
+SELECT * FROM roles
+UNION ALL
+SELECT * FROM classes
+UNION ALL
+SELECT * FROM guest_statuses
+UNION ALL
+SELECT * FROM service_statuses
 
 -- 8. Write a query that returns Guest Classes with Levels and Generate a new column with a label for their level grouping (lvl 1-10, 10-20, etc)
+SELECT (CASE WHEN [level] = 1 THEN
+'Newbie' WHEN [level] > 1 AND [level] < 10 THEN
+'Apprentice' ELSE 'Journeyman' END) as ClassTitle, * From levels
 
 -- 9. Write a series of INSERT commands that will insert the statuses of one table into another of your choosing using SELECT statements (See our lab in class - The INSERT commands should be generated). It’s ok if the data doesn’t match or make sense! :)
 -- * Remember, INSERT Commands look like: INSERT INTO Table1 (column1, column2) VALUES (column1, column2)
